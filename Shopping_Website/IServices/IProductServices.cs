92a9ee0e1b58d006f845ea10b99351a602560fab
shanghaik168@gmail.com <Shanghaik1998>
2023-03-13 16:09:23 +0700

    services

---
+++ Shopping_Website/IServices/IProductServices.cs
@@ -0,0 +1,15 @@
+﻿using Shopping_Website.Models;
+
+namespace Shopping_Website.IServices
+{
+    public interface IProductServices
+    {
+        public bool CreateProduct(Product p);
+        public bool UpdateProduct(Product p);
+        public bool DeleteProduct(Guid id);
+        public Product GetProductById(Guid id);
+        public List<Product> GetProductsByName(string name);
+        public List<Product> GetAllProducts();
+
+    }
+}
---
+++ Shopping_Website/Services/ProductServices.cs
@@ -0,0 +1,76 @@
+﻿using Shopping_Website.IServices;
+using Shopping_Website.Models;
+
+namespace Shopping_Website.Services
+{
+    public class ProductServices : IProductServices
+    {
+        ShopDbContext context;
+        public ProductServices() // Tạo constructor
+        {
+            context = new ShopDbContext();
+        }
+        public bool CreateProduct(Product p)
+        {
+            try
+            {
+                context.Products.Add(p); // Add vào Dbset
+                context.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public bool DeleteProduct(Guid id)
+        {
+            try
+            {
+                var product = context.Products.Find(id);
+                context.Products.Remove(product); // Xóa khỏi Dbset
+                context.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public List<Product> GetAllProducts()
+        {
+            return context.Products.ToList();
+        }
+
+        public Product GetProductById(Guid id)
+        {
+            return context.Products.FirstOrDefault(p => p.Id == id);
+            // return context.Products.SingleOrDefault(p => p.Id == id);
+        }
+
+        public List<Product> GetProductsByName(string name)
+        {
+            return context.Products.Where(p => p.Name == name).ToList();
+        }
+
+        public bool UpdateProduct(Product p)
+        {
+            try
+            {
+                var product = context.Products.Find(p.Id);
+                product.Name = p.Name;
+                product.Description = p.Description;
+                product.Price = p.Price;
+                product.Supplier = p.Supplier;
+                context.Update(product);    
+                context.SaveChanges(); return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
