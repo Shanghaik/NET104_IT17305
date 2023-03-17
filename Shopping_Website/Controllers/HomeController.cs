f3823de96d1b9fdf942f71ac4213adab9f13bc97
shanghaik168@gmail.com <Shanghaik1998>
2023-03-17 15:31:56 +0700

    gitnew

--- Shopping_Website/Controllers/HomeController.cs
+++ Shopping_Website/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Shopping_Website.IServices;
 using Shopping_Website.Models;
+using Shopping_Website.Services;
 using System.Diagnostics;
 
 namespace Shopping_Website.Controllers
@@ -7,10 +9,11 @@ namespace Shopping_Website.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
+        private readonly IProductServices productServices; // Khai báo interface
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
+            productServices = new ProductServices();
         }
 
         public IActionResult Index()
@@ -19,6 +22,51 @@ namespace Shopping_Website.Controllers
         }
 
         public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult Test()
+        {
+            return View(); // Khi Action return về 1 view thì có 1 request
+            // => Hiển thị ra view cùng tên với Action đó
+        }
+        public IActionResult Redirect()
+        {
+            return RedirectToAction("Test");
+        }
+        public IActionResult Show()
+        {
+            Product product = new Product()
+            {
+                Id = Guid.NewGuid(),
+                Name = "Học Lại",
+                Price = 672000,
+                AvailableQuantity = 10,
+                Supplier = "Family",
+                Description = "Học English",
+                Status = 1
+            };
+            return View(product); // Truyền tới View 1 object model
+        }
+
+        public IActionResult ShowListProduct()
+        {
+            var products = productServices.GetAllProducts();
+            return View(products); // Truyền tới View 1 object model
+        }
+
+        public IActionResult Create() // Mở form
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Product product)
+        {
+            productServices.CreateProduct(product);
+            return RedirectToAction("ShowListProduct");
+        }
+
+        public IActionResult Update()
         {
             return View();
         }
--- Shopping_Website/Program.cs
+++ Shopping_Website/Program.cs
@@ -1,7 +1,25 @@
+﻿using Shopping_Website.IServices;
+using Shopping_Website.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddTransient<IProductServices, ProductServices>();
+// builder.Services.AddSingleton<IProductServices, ProductServices>();
+// builder.Services.AddScoped<IProductServices, ProductServices>();
+/*
+ * AddSingleton: Nếu Service được khởi tạo, nó sẽ tồn tại cho đến khi vòng đời
+ * của ứng dụng kết thúc. Nếu các Request khác mà triển khai thì sẽ dùng lại 
+ * chính service đó. Phù hợp cho các services có tính toàn cục và không thay đổi
+ * AddScoped: Mỗi lần có http Request thì sẽ tạo ra services 1 lần và được giữ
+ * nguyên trong quá trình request được xử lý. Loại này sẽ được sử dụng cho các
+ * services với những yêu cầu http cụ thể.
+ * Adđ transient: Tạo mới services mỗi khi có request. Với mỗi yêu cầu http sẽ 
+ * nhận được một đối tượng services khác nhau. Phù hợp cho các services mà có thể
+ * phụ vụ nhiều yêu cầu http request.
+ */
+
 
 var app = builder.Build();
 
